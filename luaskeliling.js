2934e2548aafc26d05a4c71cb5ff9e1a6b6a2706
Agasi <agasihana29@gmail.com>
2023-09-29 21:26:37 +0700

    commit soa1

---
+++ luaskeliling.js
@@ -0,0 +1,14 @@
+exports.luaspersegi = function(sisi){
+    return sisi * sisi
+};
+exports.kelilingpersegi = function(sisi){
+    return 4 * sisi
+};
+exports.luaspersegipanjang = function(panjang, lebar){
+    return panjang * lebar
+};
+exports.kelilingpersegipanjang = function(panjang, lebar){
+    return 2 * (panjang + lebar)
+};
+
+  
