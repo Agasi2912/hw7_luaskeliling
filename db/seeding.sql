2d2943ca7e7e46c32f1b84f0c7b543966be9176d
Agasi <agasihana29@gmail.com>
2023-10-05 16:47:18 +0700

    express commit

---
+++ db/seeding.sql
@@ -0,0 +1,2 @@
+INSERT INTO actor (first_name, last_name)
+VALUES ('Jeckie', 'Chen'), ('Kim', 'So-hyun'), ('Lee', 'Jong-sook'), ('Lee', 'Min-ho'), ('Im', 'Yoona-ah')
--- soal1.js
+++
@@ -1,8 +0,0 @@
-// Local module
-const luaskeliling = require ('./luaskeliling.js')
-
-// Calculation 
-console.log("Luas persegi yang memiliki sisi bernilai 9 adalah "+  luaskeliling.luaspersegi(9) );
-console.log("Keliling persegi yang memiliki sisi bernilai 9 adalah "+ luaskeliling.kelilingpersegi(9));
-console.log("Luas persegi panjang yang memiliki panjang bernilai 12 dan lebar bernilai 5 adalah "+luaskeliling.luaspersegipanjang(12,5));
-console.log("Keliling persegi panjang yang memiliki panjang bernilai 12 dan lebar bernilai 5 adalah "+luaskeliling.kelilingpersegipanjang(12,5));
